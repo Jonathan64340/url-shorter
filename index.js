3936ce6462f3f25126e8eea053446d7cf1645de6
jonathan <you@example.comdomingues.jonathan95@gmail.com>
2022-08-13 18:36:34 +0200

    replace real config

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-const express = require('express');
-const app = express();
-const cors = require('cors');
-const routes = require('./config/routes');
-
-app.use(cors());
-app.use(routes);
-
-// listen server on port 1234
-app.listen(process.env.PORT || 1234, () => console.log('This server is started on port 1234'));
+const express = require('express');
+const app = express();
+const routes = require('./config/routes.js');
+const cors = require('cors');
+
+app.use(express.json());
+app.use(express.static('public'));
+app.use(cors());
+app.use(routes);
+console.log(process.env.DATABASE);
+app.listen(process.env.PORT || 1234, () => console.log('Server starting on port 1234'));
+
